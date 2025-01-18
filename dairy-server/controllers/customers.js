85cac8afb6c9b859a661e98dbcd2d01a1ff72197
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-18 17:08:58 +0530

    Created API Route for fetching all customers

--- dairy-server/controllers/customers.js
+++ dairy-server/controllers/customers.js
@@ -6,4 +6,14 @@ exports.addCustomer = async (req, resp) => {
     let customer = new Customer(req.body);
     let result = await customer.save();
     resp.send(result)
-  };
+};
+
+
+// Get all customers from the database
+exports.getCustomers = async (req, resp) => {
+    let customers = await Customer.find();
+    if (customers.length > 0) {
+      resp.send(customers)
+    }
+};
+
--- dairy-server/routes/customerRoutes.js
+++ dairy-server/routes/customerRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {addCustomer} = require('../controllers/customers')
+const {addCustomer, getCustomers} = require('../controllers/customers')
 
 const router = express.Router();
 
 
 router.post('/add-customer', addCustomer); // Joining the API path and function  
+router.post('/get-customer', getCustomers);
 
 module.exports = router; 
