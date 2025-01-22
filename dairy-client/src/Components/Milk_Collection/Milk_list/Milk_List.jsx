d52b34ced2b05d581fcae3fd7ad5045f2b533d88
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-22 18:44:53 +0530

    Created Component to view all milk records and integrated get-milk-list API

---
+++ dairy-client/src/Components/Milk_Collection/Milk_list/Milk_List.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { Table, Button, Form} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMilkRecords } from '../../../Slices/milkSclice';
+import './Milk_List.css'
+
+function Milk_List() {
+
+    const dispatch = useDispatch();
+    const {records} = useSelector((state) => state.milkRecords);
+
+    console.log(records)
+
+    useEffect(() => {
+          dispatch(fetchMilkRecords());
+      }, [dispatch]);
+
+  return (
+    <div className='milk_entry_list'>
+        <div className='table_box'>
+      <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Sl.no</th>
+                            <th>Date</th>
+                            <th>Member</th>
+                            <th>Shift</th>
+                            <th>Type</th>
+                            <th>Quantity</th>
+                            <th>Fat</th>
+                            <th>Total</th>
+                            <th colSpan={2}>Operations</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { 
+                            records.length > 0  ? records.map((item, index) =>
+                                <tr key={item._id}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.date}</td>
+                                    <td>{item.customerId}</td>
+                                    <td>{item.session}</td>
+                                    <td>{item.category}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>{item.fat}</td>
+                                    <td>{item.total}</td>
+                                    {/* <td className='updateBtn'><Button variant="success" onClick={() => navigate("/update/" + item._id)} type="button">Update</Button></td> */}
+                                    {/* <td className='deleteBtn'><Button variant="danger" onClick={() => deleteCustomer(item._id)} type="button">Delete</Button></td> */}
+                                </tr>) : <tr><td colSpan={6}> No result found</td></tr>
+                        } 
+                    </tbody>
+                </Table>
+                </div>
+    </div>
+  )
+}
+
+export default Milk_List
