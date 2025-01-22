cb99954128b7648987a588e264bd50210c213252
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-22 23:45:14 +0530

    Included update-fat API Route into Add-Fat

--- dairy-server/controllers/Fat_Rates_Controller.js
+++ dairy-server/controllers/Fat_Rates_Controller.js
@@ -1,16 +1,24 @@
 const FatRate = require("../Schemas/Fat_Rates_Schema")
 
 
-// Add a new Fat Rate in the database
+// Add or update Fat Rate in the database
 exports.addFatRates = async (req, res) => {
-    const { fat, rate } = req.body;
-    try {
-      const newFatRate = new FatRate({ fat, rate });
-      await newFatRate.save();
-      res.status(201).json(newFatRate);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+  const { fat, rate } = req.body;
+
+  try {
+    const existingFatRate = await FatRate.findOne({ fat });
+    if (existingFatRate) {
+      existingFatRate.rate = rate;
+      await existingFatRate.save();
+      return res.status(200).json(existingFatRate);
     }
-  };
+
+    const newFatRate = new FatRate({ fat, rate });
+    await newFatRate.save();
+    res.status(201).json(newFatRate);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 
