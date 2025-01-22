0688b56474ff3460cf085909eca68d63a085b251
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-22 15:57:09 +0530

    Added MilkRecord reducer to store

--- dairy-client/src/Redux-Store/store.js
+++ dairy-client/src/Redux-Store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import customersReducer from '../Slices/customerSlice';
+import milkReducer from '../Slices/milkSclice';
+
 
 
 export const store = configureStore({
   reducer: {
-    customers: customersReducer // Use the posts reducer to manage the posts state
+    customers: customersReducer , // Use the customer reducer to manage the customer state
+    milkrecords : milkReducer
   }
 });
