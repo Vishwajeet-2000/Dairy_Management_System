65e7038c3e05cb14665b2c2fd4a50649b1413a57
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-22 23:55:02 +0530

    Added fatRate reducer into store

--- dairy-client/src/Redux-Store/store.js
+++ dairy-client/src/Redux-Store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import customersReducer from '../Slices/customerSlice';
 import milkReducer from '../Slices/milkSclice';
-
+import fatReducer from '../Slices/fatSlice'
 
 
 export const store = configureStore({
   reducer: {
     customers: customersReducer , // Use the customer reducer to manage the customer state
-    milkRecords : milkReducer
+    milkRecords : milkReducer ,
+    fatRates : fatReducer
   }
 });
