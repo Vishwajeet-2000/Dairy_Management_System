7a53539aa66924867a29503bd8f866c976a80d97
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-23 16:33:49 +0530

    added rate field to milkRecord Schema

--- dairy-server/Schemas/milkSchema.js
+++ dairy-server/Schemas/milkSchema.js
@@ -5,12 +5,13 @@ const mongoose = require('mongoose');
 const milkRecordSchema = new mongoose.Schema({
   customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customers'},
   customerName: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  date: { type: Date },
   session: { type: String, enum: ['Morning', 'Evening']},
   category: { type: String, enum: ['Cow', 'Buffalo']},
   quantity: { type: Number },
   fat: { type: Number},
+  rate: {type:Number},
   total: { type: Number },
 },{ timestamps: true });
 
-module.exports = mongoose.model('MilkRecords', milkRecordSchema);    
+module.exports = mongoose.model('MilkRecords', milkRecordSchema);
