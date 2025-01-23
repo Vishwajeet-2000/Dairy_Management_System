b31727be2cace3ba744d947555b9413f8ecf4b4d
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-23 10:35:13 +0530

    Designed the Add-Fat Form

--- dairy-client/src/Components/Fat_Rates/AddFat.jsx
+++ dairy-client/src/Components/Fat_Rates/AddFat.jsx
@@ -1,27 +1,43 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 import { useDispatch } from 'react-redux';
-import { addOrUpdateFatRate } from './features/fatRates/fatRateSlice';
+import { addOrUpdateFatRate } from '../../Slices/fatSlice';
 
 function AddFat() {
     const dispatch = useDispatch();
     const [fat, setFat] = useState('');
     const [rate, setRate] = useState('');
+
+    const [success, setSuccess] = useState(true); 
   
-    const handleSubmit = () => {
+    const handleAddFat = () => {
       dispatch(addOrUpdateFatRate({ fat: parseFloat(fat), rate: parseFloat(rate) }));
-      setFat('');
-      setRate('');
+ 
+      setTimeout(() => {
+        setSuccess(true)
+        setFat('')
+        setRate('')
+      }, 1200)
     };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-      <input type="number" step="0.1" placeholder="Enter Fat" value={fat} onChange={(e) => setFat(e.target.value)} required />
+    <div className='add_fat'>
+    <Form>
+      {success ? <h1 className='mt-5'>Add / Update Fat</h1> : <Alert className='mt-5' variant="success"><Alert.Heading>Fat added succesfully</Alert.Heading></Alert>}
+      <Form.Group className="mb-2 mt-4">
+        <Form.Label>Fat</Form.Label>
+        <Form.Control type="number" step="0.30" value={fat} onChange={(e) => setFat(e.target.value)} placeholder="Enter Fat" required/>
+      </Form.Group>
 
-      <input type="number" step="0.01" placeholder="Enter Rate" value={rate} onChange={(e) => setRate(e.target.value)} required />
+      <Form.Group className="mb-2 mt-4">
+        <Form.Label>Rate</Form.Label>
+        <Form.Control type="number" step="0.30" value={rate} onChange={(e) => setRate(e.target.value)} placeholder="Enter rate" required/>
+      </Form.Group>
 
-      <button type="button" onClick={() => handleAddFat()}>Add/Update Fat Rate</button>
-    </form>
+      <Button variant="primary" onClick={() => { handleAddFat() ; setSuccess(false)}} type="button">Add / Update Fat</Button>
+    </Form>
     </div>
   )
 }
