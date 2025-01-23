f971ad8bd2d6d90562a873e0719ddc382188c71f
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2025-01-23 13:31:19 +0530

    added date field to milkSchema

--- dairy-server/Schemas/milkSchema.js
+++ dairy-server/Schemas/milkSchema.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 
 const milkRecordSchema = new mongoose.Schema({
   customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customers'},
-  // date: { type: Date, default: Date.now },
+  customerName: { type: String, required: true },
+  date: { type: Date, default: Date.now },
   session: { type: String, enum: ['Morning', 'Evening']},
   category: { type: String, enum: ['Cow', 'Buffalo']},
   quantity: { type: Number },
   fat: { type: Number},
   total: { type: Number },
-//   createdAt: { type: Date, default: Date.now }
 },{ timestamps: true });
 
 module.exports = mongoose.model('MilkRecords', milkRecordSchema);    
